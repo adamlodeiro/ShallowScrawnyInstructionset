6270615f929be30e5709bc7f85749a5e4f5c3392
adamlodeiro <adam.lodeiro@patelhs.org>
2020-09-22 14:09:17 +0000

    Here I put all declerations for the ints and I then typed out "I am 15 years old" one change I made was change 0921 to 921 because the code said the int was too large.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     int myAge = 15;
     int myBirthDay = 0731;
-    int todayDate = 921;
+    int todaysDate = 921;
     System.out.println("I am " + myAge + " years old.");
   }
 }
